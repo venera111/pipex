481a3681fb15625199aa317d3bac30a8792138c8
venera111 <lysovayana@mail.ru>
2022-01-22 20:17:41 +0300

    up

--- pipex.c
+++ pipex.c
@@ -6,106 +6,104 @@
 /*   By: qestefan <qestefan@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 13:45:05 by qestefan          #+#    #+#             */
-/*   Updated: 2022/01/22 17:10:05 by qestefan         ###   ########.fr       */
+/*   Updated: 2022/01/22 20:12:14 by qestefan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char	*get_path(char **cmd, char **envp)
+void	pars_envp(char **envp, char	**cmd, int i, int k)
 {
-	int		i;
+	char	**out;
 	char	*path;
-	char	**paths;
-	char	*part_path;
 
-	i = 0;
-	while (!ft_strnstr(envp[i], "PATH=", 5))
+	while (envp[i])
 	{
+		path = ft_strnstr(envp[i], "PATH=", 5);
+		if (path)
+		{
+			path = ft_strrchr(path, '=');
+			out = ft_split(path, ':');
+			while (out[k])
+			{
+				i = 0;
+				out[k] = ft_strjoin(out[k], "/");
+				out[k] = ft_strjoin(out[k], cmd[0]);
+				execve(out[k], cmd, NULL);
+				k++;
+			}
+			ft_perror("Wrong command");
+		}
 		i++;
-		if (!envp[i])
-			ft_perror(PATH_ERROR);
 	}
-	paths = ft_split(envp[i] + 5, ':');
-	i = 0;
-	while (paths[i])
-	{
-		part_path = ft_strjoin(paths[i], "/");
-		path = ft_strjoin(part_path, cmd[0]);
-		if (access(path, F_OK) == 0)
-			return (path);
-		i++;
-	}
-	return (0);
 }
 
-void	child_process(char **envp, int *fd, t_data *data)
+void	child_process(t_data *data, char **envp, int *fd)
 {
-	char *path;
+	int	i;
+	int	k;
 
-	if (dup2(data->file_fd[0], 0) < 0)
-		ft_perror(DUP2_ERROR);
+	i = 0;
+	k = 0;
+	close(fd[0]);
+	if (dup2(data->file1, 0) < 0)
+		ft_perror(DUP2_FILE);
 	if (dup2(fd[1], 1) < 0)
 		ft_perror(DUP2_PIPE);
-	close(fd[0]);
 	close(fd[1]);
-	close(data->file_fd[0]);
-	path = get_path(data->cmd1, envp);
-	execve(path, data->cmd1, envp);
+	close(data->file1);
+	pars_envp(envp, data->cmd1, i, k);
 }
 
-void	parent_process(char **envp, int *fd, t_data *data)
+void	parent_process(t_data *data, char **envp, int *fd)
 {
-	char *path;
+	int	i;
+	int	k;
 
-	if (dup2(data->file_fd[1], 1) < 0)
-		ft_perror(DUP2_ERROR);
+	i = 0;
+	k = 0;
+	close(fd[1]);
 	if (dup2(fd[0], 0) < 0)
 		ft_perror(DUP2_PIPE);
+	if (dup2(data->file2, 1) < 0)
+		ft_perror(DUP2_FILE);
 	close(fd[0]);
-	close(fd[1]);
-	close(data->file_fd[1]);
-	path = get_path(data->cmd2, envp);
-	execve(path, data->cmd2, envp);
+	close(data->file2);
+	pars_envp(envp, data->cmd2, i, k);
 }
 
-void	check_argv(int argc, char **argv, t_data *data)
+void	check_argv(int argc, t_data *data, char **argv)
 {
 	if (argc == 5)
 	{
 		data->cmd1 = ft_split(argv[2], ' ');
 		data->cmd2 = ft_split(argv[3], ' ');
-		if (access(argv[1], F_OK) != 0 || access(argv[1], R_OK) != 0)
-			ft_perror(NO_FILE);
-		data->file_fd[0] = open(argv[1], O_RDONLY, 0777);
-		data->file_fd[1] = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
-		if (data->file_fd[0] == -1 || data->file_fd[1] == -1)
+		data->file1 = open(argv[1], O_RDONLY);
+		data->file2 = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+		// dprintf(2, "%d %d\n", data->file1, data->file2);
+		if (data->file1 == -1 || data->file2 == -1)
 			ft_perror(FILE_ERROR);
 	}
 	else
-		ft_perror(ARGV_ERROR);
+		ft_perror(ARGC_ERROR);
+
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	t_data	data;
-	int		fd[2];
 	int		pid;
+	int		fd[2];
+	t_data	data;
 
-	if (envp[0] == NULL)
-		return (EXIT_SUCCESS);
-	check_argv(argc, argv, &data);
 	if (pipe(fd) == -1)
 		ft_perror(PIPE_ERROR);
+	check_argv(argc, &data, argv);
 	pid = fork();
 	if (pid == -1)
 		ft_perror(PID_ERROR);
 	if (pid == 0)
-		child_process(envp, fd, &data);
+		child_process(&data, envp, fd);
 	else
-	{
-		waitpid(pid, NULL, 0);
-		parent_process(envp, fd, &data);
-	}
+		parent_process(&data, envp, fd);
 	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -6,17 +6,17 @@
 /*   By: qestefan <qestefan@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 14:22:48 by qestefan          #+#    #+#             */
-/*   Updated: 2022/01/22 17:07:47 by qestefan         ###   ########.fr       */
+/*   Updated: 2022/01/22 18:01:44 by qestefan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-#include <stdlib.h> // EXIT_FAILURE
-#include <fcntl.h> // O_RDONLY
-#include <unistd.h> // pipe()
-#include <stdio.h> // perror()
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
 #include "error.h"
 #include "libft/libft.h"
 
@@ -25,7 +25,8 @@ typedef struct s_data
 	pid_t	pid;
 	char	**cmd1;
 	char	**cmd2;
-	int		file_fd[2];
+	int		file1;
+	int		file2;
 
 }	t_data;
 
